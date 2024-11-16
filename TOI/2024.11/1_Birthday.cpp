ef47411e831ed3613033ef617738a7ff0852f07d
Andy8787(布長) <andy8787main@gmail.com>
2024-11-17 00:44:34 +0800

    更改變數名

--- TOI/2024.11/1_Birthday.cpp
+++ TOI/2024.11/1_Birthday.cpp
@@ -4,20 +4,20 @@ using namespace std;
 int main() {
     int arr_day[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-    int Mouth1, Day1;
-    cin >> Mouth1 >> Day1;
+    int Month1, Day1;
+    cin >> Month1 >> Day1;
 
     int num1 = 0;
-    for (int i = 0; i < Mouth1 - 1; i++) {
+    for (int i = 0; i < Month1 - 1; i++) {
         num1 += arr_day[i];
     }
     num1 += Day1;
 
-    int Mouth2, Day2;
-    cin >> Mouth2 >> Day2;
+    int Month2, Day2;
+    cin >> Month2 >> Day2;
 
     int num2 = 0;
-    for (int i = 0; i < Mouth2 - 1; i++) {
+    for (int i = 0; i < Month2 - 1; i++) {
         num2 += arr_day[i];
     }
     num2 += Day2;
