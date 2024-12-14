814f1e8ebf60ce800879b6b57e79fac40cc165a7
Andy8787(布長) <andy8787main@gmail.com>
2024-12-14 13:33:56 +0800

    久違寫codewars

---
+++ Codewars/(8)Who ate the cookie.py
@@ -0,0 +1,15 @@
+# https://www.codewars.com/kata/55a996e0e8520afab9000055/train/python
+
+def cookie(x):
+    if (type(x) == type("Ryan")):
+        return "Who ate the last cookie? It was Zach!"
+    
+    if (type(x) == type(2.3)):
+        return "Who ate the last cookie? It was Monica!"
+    
+    if (type(x) == type(26)):
+        return "Who ate the last cookie? It was Monica!"
+    
+    return "Who ate the last cookie? It was the dog!"
+    
+    
